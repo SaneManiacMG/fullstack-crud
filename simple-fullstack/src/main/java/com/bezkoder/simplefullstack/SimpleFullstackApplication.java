8e92ab82035546671b7c4936b3b8e456601b8a1f
SaneManiacMG <mmoroane@hotmail.com>
2022-12-05 15:06:29 +0200

    Init Commit java

---
+++ simple-fullstack/src/main/java/com/bezkoder/simplefullstack/SimpleFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.bezkoder.simplefullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFullstackApplication.class, args);
+	}
+
+}
