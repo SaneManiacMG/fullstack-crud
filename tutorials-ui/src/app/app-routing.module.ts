ac97c3d9f2178be60ca70bed111d4626e440f781
SaneManiacMG <mmoroane@hotmail.com>
2022-12-06 10:12:34 +0200

    Added routing for components

--- tutorials-ui/src/app/app-routing.module.ts
+++ tutorials-ui/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'tutorials', component: TutorialDetailsComponent },
+  { path: 'tutorials/:id', component: TutorialsListComponent },
+  { path: 'tutorials', component: AddTutorialComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
