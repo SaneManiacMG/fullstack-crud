2e369d25660c1002773d075399fb707d0654dc9d
SaneManiacMG <mmoroane@hotmail.com>
2022-12-06 13:50:09 +0200

    Imported and installed bootsrap Added tutorials list code html and ts

--- tutorials-ui/src/app/components/tutorials-list/tutorials-list.component.ts
+++ tutorials-ui/src/app/components/tutorials-list/tutorials-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
+import { Tutorial } from 'src/app/models/tutorial.model';
+import { TutorialService } from 'src/app/services/tutorial.service';
 
 @Component({
   selector: 'app-tutorials-list',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./tutorials-list.component.css']
 })
 export class TutorialsListComponent {
+  tutorials?: Tutorial[];
+  currentTutorial: Tutorial = {};
+  currentIndex = -1;
+  title = '';
 
+  constructor(private tutorialService: TutorialService) {}
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.tutorialService.getAll().subscribe({
+      next: (data) => {
+        this.tutorials = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e)
+    });
+  }
+
+  refreshList(): void {
+    this.retrieveTutorials();
+    this.currentTutorial = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveTutorial(tutorial: Tutorial, index: number): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllTutorials(): void {
+    this.tutorialService.deleteAll().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.refreshList();
+      },
+      error: (e) => console.error(e)
+    })
+  }
+
+  searchTitle(): void {
+    this.currentTutorial = {};
+    this.currentIndex = -1;
+
+    this.tutorialService.findByTitle(this.title).subscribe({
+      next: (data) => {
+        this.tutorials = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e)
+    });
+  }
 }
