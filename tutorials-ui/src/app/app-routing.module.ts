c1120e276930061d4b03e47675cf43ec99ee6b3d
SaneManiacMG <mmoroane@hotmail.com>
2022-12-06 10:57:54 +0200

    Corrected routing rules Removed generated html

--- tutorials-ui/src/app/app-routing.module.ts
+++ tutorials-ui/src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.com
 
 const routes: Routes = [
   { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
-  { path: 'tutorials', component: TutorialDetailsComponent },
-  { path: 'tutorials/:id', component: TutorialsListComponent },
+  { path: 'tutorials/:id', component: TutorialDetailsComponent },
+  { path: 'tutorials', component: TutorialsListComponent },
   { path: 'tutorials', component: AddTutorialComponent }
 ];
 
